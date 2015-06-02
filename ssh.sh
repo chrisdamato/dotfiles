7bb6307edf81f3d91852dd2a4db687c5c533001e
Chris D'Amato (scrod) <chris@chrisdamato.com>
2015-06-02 09:32:55 -0400

    pd

--- ssh.sh
+++ ssh.sh
@@ -1,12 +1,13 @@
 function ssh() { 
     AARGH=""
+    OPT=""
     while (( $# )); do 
         if [[ "$1" == "pd" ]] ; then
             OPT="-o 'proxycommand ssh hq nc %h %p'"
         elif [[ "$1" =~ ^[0-9]+$ ]]; then
-            AARGH="${AARGH} 10.18.3.$1"
+            AARGH="${AARGH}${AARGH+ }10.18.3.$1"
         else
-            AARGH="${AARGH} $1"
+            AARGH="${AARGH}${AARGH+ }$1"
         fi
         shift
     done;
